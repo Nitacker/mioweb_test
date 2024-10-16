0afe894fc704780f73a6cc5b224245a64423d9fb
Nitacker <kvapil02@gmail.com>
2024-10-16 18:57:27 +0200

    2nd
    totalPrice not showing

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 function updatePrice () {
     //selektor itemu a načtení kvantity
     var selectedItem = document.querySelector('input[name="product"]:checked');
-    //var price = document.getElementById('price');
+    var price = document.getElementById('price');
     var quantity = document.getElementById('quantity');
 
     if (selectedItem) {
         //načtení ceny vybraného itemu
         var itemPrice = selectedItem.getAttribute('data-price');
-        //priceElement.value = productPrice;
+        price.value = itemPrice;
 
         // výpočet celkové ceny a zobrazení
-        var totalCost = itemPrice* quantity; 
-        document.getElementById('total').textContent = totalCost.toFixed(2) + CZK; 
+        var total = itemPrice* quantity; 
+        document.getElementById('totalPrice').textContent = total.toFixed(2) + CZK; 
     }
 }
